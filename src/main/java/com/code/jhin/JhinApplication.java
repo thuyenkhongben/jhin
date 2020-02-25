274221ffd9d6aa3b6fa13794eb9fa060ad5b7463
unknown <you@example.com>
2020-02-25 13:22:14 +0700

    cap nhat 1

---
+++ src/main/java/com/code/jhin/JhinApplication.java
@@ -0,0 +1,27 @@
+package com.code.jhin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+        JhinApplication.class,
+        Jsr310JpaConverters.class
+})
+public class JhinApplication {
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/jhin/JhinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.jhin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
