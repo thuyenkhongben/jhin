8d62ffea87e3b0235d2f9799b9454d2088c6bdc9
unknown <you@example.com>
2020-03-02 16:14:00 +0700

    test

---
+++ src/main/java/com/code/jhin/model/Product.java
@@ -0,0 +1,11 @@
+package com.code.jhin.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
